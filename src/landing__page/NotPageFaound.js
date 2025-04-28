6c1a6b1b9f116e716acf1601ad29bd1c2858a257
PawarSunil97 <pawarsunil281297@gmail.com>
2025-04-28 13:53:15 +0530

    added product page

---
+++ src/landing__page/NotPageFaound.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const NotPageFaound = () => {
+    return (
+        <div className='container p-5'>
+          <div className='row text-center'>
+           <h1 className=' fs-2 mt-5'>404 - Page Not Found</h1>
+           <p>Sorry, the page you are looking for could not be found.</p>
+           
+          </div>
+          
+        </div>
+      )
+};
+
+export default NotPageFaound;
---
+++ src/landing__page/appConstant.js
@@ -0,0 +1,71 @@
+export const PRODUCTPAGELEFT = {
+  PRODUCTPAGERIGHTFIRST: {
+    IMAGE_URL: "media/images/kite.png",
+    PRODUCT_NAME: "Kit",
+    PRODUCT_DISCRIPTION:
+      "Our ultra-fast flagship trading platform with streaming market data, advanced charts, an elegant UI, and more. Enjoy the Kite experience seamlessly on your Android and iOS devices.",
+    TRY_DEMO: "Try Demo →",
+    LEARN_MORE: "Learn More →",
+    GOOGLE_PLAY_IMG: "media/images/googlePlayBadge.svg",
+    GOOGLE_PLAY_ALT_TEXT: "googlePlay",
+    APP_STORE_IMG: "media/images/appstoreBadge.svg",
+    APP_STORE_ALT_TEXT: "appStore",
+  },
+  PRODUCTPAGERIGHTSECOND: {
+    IMAGE_URL: "media/images/coin.png",
+    PRODUCT_NAME: "Coin",
+    PRODUCT_DISCRIPTION:
+      "Buy direct mutual funds online, commission-free, delivered directly to your Demat account. Enjoy the investment experience on your Android and iOS devices.",
+    TRY_DEMO: "Coin →",
+    GOOGLE_PLAY_IMG: "media/images/googlePlayBadge.svg",
+    GOOGLE_PLAY_ALT_TEXT: "googlePlay",
+    APP_STORE_IMG: "media/images/appstoreBadge.svg",
+    APP_STORE_ALT_TEXT: "appStore",
+  },
+  PRODUCTPAGERIGHTTHIRD: {
+    IMAGE_URL: "media/images/varsity.png",
+    PRODUCT_NAME: "Varsity mobile",
+    PRODUCT_DISCRIPTION:
+      "An easy to grasp, collection of stock market lessons with in-depth coverage and illustrations. Content is broken down into bite-size cards to help you learn on the go.",
+    GOOGLE_PLAY_IMG: "media/images/googlePlayBadge.svg",
+    GOOGLE_PLAY_ALT_TEXT: "googlePlay",
+    APP_STORE_IMG: "media/images/appstoreBadge.svg",
+    APP_STORE_ALT_TEXT: "appStore",
+  },
+};
+
+export const PRODUCTPAGERIGHT = {
+  PRODUCTPAGERIGHTCONSOLE: {
+    IMAGE_URL_CONSOLE: "media/images/console.png",
+    IMG_ALT: "console",
+    PRODUCT_NAME_CONSOLE: "Console",
+    PRODUCT_DISCRIPTION_CONSOLE:
+      "The central dashboard for your Zerodha account. Gain insights into your trades and investments with in-depth reports and visualisations.",
+
+      LEARN_MORE_CONSOLE: "Learn More →",
+  },
+  PRODUCTPAGERIGHT_KIT: {
+    IMAGE_URL_KITE: "media/images/kiteconnect.png",
+    KITE_IMG_ALT: "kit",
+    PRODUCT_NAME_KITE: "Kite Connect API",
+    PRODUCT_DISCRIPTION_KITE:
+      "Build powerful trading platforms and experiences with our super simple HTTP/JSON APIs. If you are a startup, build your investment app and showcase it to our clientbase.",
+
+      LEARN_MORE_KITE: "Kite Connect →",
+  },
+};
+
+export const PRODUCT_UNIVERSE= {
+  
+    FIRST_PARAGRAPH:'Want to know more about our technology stack? Check out the',
+    ZERODHA_TECH_INFO:'Zerodha.tech blog.',
+   UNIVERSE_HEADING:'Zerodha Fund House',
+
+   SECOND_PARA:'Extend your trading and investment experience even further with our partner platforms.',
+   IMAGE_URL_ZERODHA_FOUND: "media/images/zerodhaFundhouse.png",
+   IMAGE_URL_SENSIBULL: "media/images/sensibullLogo.svg",
+   IMAGE_URL_STIEAK_LOGO: "media/images/streakLogo.png",
+   IMAGE_URL_SMALLCASE:"media/images/smallcaseLogo.png",
+   IMAGE_URL_DITTO:"media/images/dittoLogo.png",
+   SIGN_BUTTON:"Sign Up For Free"
+}
